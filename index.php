9a2b141676ea1d2a32a842bf35639432dd636cfb
Amit Prajapati <amit19851207@gmail.com>
2017-11-18 16:18:55 +0545

    index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Himalayan Bank</title>
+</head>
+<body>
+
+	<Form method="post" action="https://hblpgw.2c2p.com/HBLPGW/Payment/Payment/Payment">
+		<input type="text" id="paymentGatewayID" name="paymentGatewayID" value="9100931211"/>
+		<input type="text" id="invoiceNo" name="invoiceNo" value="00000001234567890333"/>
+		<input type="text" id="productDesc" name="productDesc" value="Test Product"/>
+		<input type="text" id="amount" name="amount" value="000000001000"/>
+		<input type="text" id="currencyCode" name="currencyCode" value="524"/>
+		<input type="text" id="userDefined1" name="userDefined1" value="custom data"/>
+		<input type="text" id="nonSecure" name="nonSecure" value="Y"/>
+		<input type="text" id="hashValue" name="hashValue" value="QXXREXMCT2AC1ZEGN4FZIL28PD96UTHV"/>
+		<button type="submit">Submit</button>
+	</Form>
+
+</body>
+</html>
