75a10f76bbc3371f7c490d0e2a2965d4c62c40ed
Amit Prajapati <amit19851207@gmail.com>
2019-06-08 18:20:33 +0545

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
 <body>
 
 	<Form method="post" action="https://hblpgw.2c2p.com/HBLPGW/Payment/Payment/Payment">
-		<input type="text" id="paymentGatewayID" name="paymentGatewayID" value="9100931211"/>
+		<input type="text" id="paymentGatewayID" name="paymentGatewayID" value="9100XXXXXX"/>
 		<input type="text" id="invoiceNo" name="invoiceNo" value="00000001234567890333"/>
 		<input type="text" id="productDesc" name="productDesc" value="Test Product"/>
 		<input type="text" id="amount" name="amount" value="000000001000"/>
 		<input type="text" id="currencyCode" name="currencyCode" value="524"/>
 		<input type="text" id="userDefined1" name="userDefined1" value="custom data"/>
 		<input type="text" id="nonSecure" name="nonSecure" value="Y"/>
-		<input type="text" id="hashValue" name="hashValue" value="QXXREXMCT2AC1ZEGN4FZIL28PD96UTHV"/>
+		<input type="text" id="hashValue" name="hashValue" value="QXXREXXXXXXXXXXXXXXXXXXXXX"/>
 		<button type="submit">Submit</button>
 	</Form>
 
